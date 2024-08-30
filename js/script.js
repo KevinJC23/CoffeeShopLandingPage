93539eba2bfa6e02be89805a3cc48b35056980c7
KevinJC23 <kevinjuancarlos7@gmail.com>
2024-08-30 23:03:14 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let search = document.querySelector('.search-box');
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#search-icon').onclick = () => {
+    search.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+document.querySelector('#menu-icon').onclick = () => {
+    navbar.classList.toggle('active');
+    search.classList.remove('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.toggle('active');
+    search.classList.remove('active');
+}
+
+let header = document.querySelector('header');
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
+
+let news = document.querySelector('.news');
+document.querySelector('#news-icon').onclick = () => {
+    news.classList.toggle('active');
+    navbar.classList.remove('active');
+}
